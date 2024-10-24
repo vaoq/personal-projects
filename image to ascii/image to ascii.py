e2a551aa121e648d95dc7f6c9e7d724eebda8aa6
PEACOCK\creece <creece@misoft.co.uk>
2024-10-24 14:30:14 +0100

    holy hell this version is goated and incredibly clean for what it was before

--- image to ascii/image to ascii.py
+++ image to ascii/image to ascii.py
@@ -1,5 +1,5 @@
 from PIL import Image
-from PIL import ImageGrab
+from PIL import ImageGrab # for the future <3
 
 
 def get_image(
@@ -29,11 +29,7 @@ def image_to_ascii_art(
     image_width = rgb.size[0] // size
 
     # characters to use for the conversion
-    chars = ['@&%QWNM0gB$#DR8mHXKAUbGOpV4d9h6PkqwSE2]ayjxY5Zoen[ult13If}C{iF|(7J)vTLs?z/*cr!+<>;=^,_:\'        ',
-             '@#XOhSje1{J?!^- ',
-             '█▓░',
-             '█',
-             ' '][abs(int(char_preset))][::-1 if char_preset[0] == '-' else 1]
+    chars = ['@&%QWNM0gB$#DR8mHXKAUbGOpV4d9h6PkqwSE2]ayjxY5Zoen[ult13If}C{iF|(7J)vTLs?z/*cr!+<>;=^,_:\'        ', '@#XOhSje1{J?!^- ', '█▓░', '█', ' '][abs(int(char_preset))][::-1 if char_preset[0] == '-' else 1]
 
     # if bg is a list assign colour ansi code, else assign to nothing
     bg = (f'\x1b[48;2;{';'.join([str(background_color_rgb[i])for i in range(3)])}m'if isinstance(background_color_rgb, list)else'')
@@ -41,18 +37,13 @@ def image_to_ascii_art(
     # assign each pixel to a character
     new_pixels = [chars[(int(pixel/(255//len(chars)+1)))] for pixel in pixels]
 
-    for i in range(len(new_pixels)):
-        new_pixels[i] = f'\x1b[38;2;{colors[i][0]};{colors[i][1]};{colors[i][2]}m{new_pixels[i]}'
-        if i % image_width == 0 and i!=0:
-            print('\033[1;31;49m')
-        print(bg+new_pixels[i], end=new_pixels[i])
+    ascii_image = ''
+    for i in range(0, len(new_pixels)):
+        ascii_image += ('\033[1;31;49m\n' if i % image_width == 0 and i!=0 else '') + bg + f'\x1b[38;2;{colors[i][0]};{colors[i][1]};{colors[i][2]}m{new_pixels[i]}'*2
 
-    #screenshot = ImageGrab.grab(all_screens=True)
-    #screenshot.save('ss.png', 'PNG')
+    return ascii_image + '\033[1;31;49m'
 
-    return ''
-
-image_to_ascii_art("images\\example3.jpg",     # Directory Path of image to convert
+print(image_to_ascii_art("images\\example2.png",     # Directory Path of image to convert
                    [0,0,0],      # RGB Value of background (Hint: Typically looks better with black background)
-                   30,                    # Size of image (Hint: Higher value = Lower Resolution)
-                   '-3')            # Which set of characters to use in the 'chars' array.
+                   4,                    # Size of image (Hint: Higher value = Lower Resolution)
+                   '-3'))          # Which set of characters to use in the 'chars' array.
